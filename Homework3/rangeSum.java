41798586be4a7c7c8637ae95c487d6d2904c2ca7
Jorger93 <jorger.1993.2012@gmail.com>
2014-02-07 23:26:35 -0600

    Update rangeSum.java

--- Homework3/rangeSum.java
+++ Homework3/rangeSum.java
@@ -10,11 +10,14 @@ Dados 2 numeros (limite inferior y superior), recorra el rango e imprima los num
 public class rangeSum {   
 
    public static void main(String[] args) {
+   	
+   		int limbajo=8;
+		int limalto=17;
 
-		System.out.println("limites: "+args[0]+" "+args[1]);
+		System.out.println("limites: "+ limbajo +" "+ limalto );
 
-      
-         System.out.println("numero actual es:" );
+      do {
+	lb++; System.out.println("numero actual es:" + limbajo ); } while (lb<=la);
       
    }
 
