5918508df88b3a1dd35ff3f883cfb3d264beeee1
Jorger93 <jorger.1993.2012@gmail.com>
2014-02-08 23:07:27 -0600

    Update whileValue.java

--- Homework3/whileValue.java
+++ Homework3/whileValue.java
@@ -9,13 +9,16 @@ Dado un numero mayor a cero, imprima el valor actual desde cero hasta antes de l
 */
 
 public class whileValue {   
-
+   public static int masuno(int num){ int c=num + 1; return c;}
+   public static int menosuno(int num){ int d=num - 1; return d;}
    public static void main(String[] args) {
-   		int valorActual = 0;
+   		int valorActual = -1;
+   		int valorFinal = 21;
+   		int limiteF = menosuno(valorFinal);
 
    		//si valor actual < args[0]
-   			while( valorActual < 21 ) 
-   			{System.out.println("el valor actual es: "+valorActual); vA++; }
+   			while( valorActual < limiteF ) 
+   			{valorActual=masuno(valorActual); System.out.println("el valor actual es: "+valorActual);}
       
    }
 
